3230c9be56e4521a7847364cb7d5db63a8d6289c
iegpeppino <iegpeppino@outlook.com.ar>
2025-06-16 15:05:14 +0000

    A2 (foundation): Add messages string list to use prompt history, include --verbose argument to print additional information along with the response

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
-import  os
+import  os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-# Load environment variables
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main ():
+    
+    # Load environment variables
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-# Create instance of ai client using Gemini Api key
-client = genai.Client(api_key= api_key)
+    # Create instance of ai client using Gemini Api key
+    client = genai.Client(api_key= api_key)
 
-# Generate a response object using the ai model
-response = client.models.generate_content(
-    model= "gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-)
+    if len(sys.argv) < 1:
+        print("Please provide a prompt to execute")
+        print("Exiting program")
+        exit(1)
+   
+    user_prompt = sys.argv[1]
 
-print(response.text)
-print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    # Create list of message history
+    messages = [
+        types.Content(role= "user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    # Generate a response object using the ai model
+    response = client.models.generate_content(
+        model= "gemini-2.0-flash-001",
+        contents= messages,
+    )
+
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "--verbose":
+            print(f'User prompt: {user_prompt}')
+            print(response.text)
+            print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+            print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    else:
+        print(response.text)
+
+    
+if __name__ == "__main__":
+    main()
