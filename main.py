d8a5cbb152b5a0388269156009879993dc168e9e
iegpeppino <iegpeppino@outlook.com.ar>
2025-06-19 00:17:43 +0000

    C5 (functions): Create call_funcion to call available functions from a function dictionary, catch their responses and return them

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from function_schemas import schema_get_file_content, schema_run_python_file, schema_write_file, schema_get_files_info
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+from functions.write_file import write_file
+from functions.run_python_file import run_python_file
+
 
 def main ():
 
@@ -19,6 +24,7 @@ def main ():
         exit(1)
    
     user_prompt = sys.argv[1]
+    verbose = "--verbose" in sys.argv
 
     #
     system_prompt = """
@@ -58,11 +64,18 @@ def main ():
             system_instruction=system_prompt),
     )
 
+
     if len(response.function_calls) > 0:
         for function_call_part in response.function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+            function_call_result = call_function(function_call_part, verbose)
+            if not function_call_result.parts[0].function_response.response:
+                raise ValueError("blablabla")
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+                
+            
     if len(sys.argv) > 2:
-        if sys.argv[2] == "--verbose":
+        if verbose:
             print(f'User prompt: {user_prompt}')
             print(response.text)
             print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
@@ -70,6 +83,47 @@ def main ():
     else:
         print(response.text)
 
+    
+def call_function(function_call_part, verbose=False):
+
+    function_name = function_call_part.name
+    args = function_call_part.args
+    working_dir = "./calculator"
+
+    func_dict = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "write_file": write_file,
+        "run_python_file": run_python_file,
+    }
+    
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+    
+    if function_call_part.name not in func_dict.keys():
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error":f"unknown function: {function_name}"}
+                )
+            ]
+        )
+    
+    function_result = func_dict[function_name](working_directory= working_dir, **args)
+
+    return types.Content(
+    role="tool",
+    parts=[
+        types.Part.from_function_response(
+            name=function_name,
+            response={"result": function_result},
+            )
+        ],
+    )
     
 if __name__ == "__main__":
     main()
